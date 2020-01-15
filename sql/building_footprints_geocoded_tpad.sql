88724d0cd2bb36c959132372d3af62a6b5d5f083
Ann Morris <annelizabethmorris@yahoo.com>
2020-01-15 11:53:48 -0500

    check messages in addition to return codes

--- sql/building_footprints_geocoded_tpad.sql
+++ sql/building_footprints_geocoded_tpad.sql
@@ -4,4 +4,6 @@ geo_tpad_dm_bin_status, geo_tpad_conflict_flag, geo_tpad_bin_status,
 geo_return_code, geo_return_code_2, geo_message, geo_message_2, geo_reason_code
 INTO building_footprints_geocoded_tpad
 FROM building_footprints_geocoded
-WHERE geo_return_code IN ('22', '23');
+WHERE geo_return_code IN ('22', '23')
+OR geo_message LIKE '%TPAD%'
+OR geo_message_2  LIKE '%TPAD%';
